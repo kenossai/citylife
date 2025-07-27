ccdf0fa6e0e6234576c9fb08058d7a3bfa529f3a
Kenneth Ossai <kenossai1@gmail.com>
2025-07-27 14:45:46 +0100

    Disabled preloader

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -48,9 +48,9 @@
 <div class="custom-cursor__cursor"></div>
     <div class="custom-cursor__cursor-two"></div>
 
-    <div class="preloader">
+    {{-- <div class="preloader">
         <div class="preloader__image" style="background-image: url(asset('assets/images/loader.png'));"></div>
-    </div>
+    </div> --}}
 
     {{ $slot }}
 
