46e463e12c97b74562a983c83f34216f1045443f
Kenneth Ossai <kenossai1@gmail.com>
2025-08-13 08:04:48 +0100

    Update MediaController to provide series data to view
    The MediaController@index method now retrieves featured and latest teaching series, as well as categories, and passes them to the 'pages.media.index' view. This prepares the media overview page to display dynamic content.

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\TeachingSeries;
 
 class MediaController extends Controller
 {
     public function index()
     {
-        // Logic to display media items
-        return view('pages.media.teaching-series');
+        // Get featured and latest teaching series for the media overview
+        $featuredSeries = TeachingSeries::getFeaturedSeries(6);
+        $latestSeries = TeachingSeries::getLatestSeries(8);
+        $categories = TeachingSeries::getCategories();
+        
+        return view('pages.media.index', compact('featuredSeries', 'latestSeries', 'categories'));
     }
 }
