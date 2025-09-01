93257aeec5d1a3417ebaa49daa1495727e22c8ff
Kenneth Ossai <kenossai1@gmail.com>
2025-09-01 15:02:41 +0100

    Update paypal.php

--- config/paypal.php
+++ config/paypal.php
@@ -5,7 +5,7 @@ return [
     'sandbox_button_id' => env('PAYPAL_SANDBOX_BUTTON_ID', 'TEST_BUTTON_ID'),
     'live_button_id' => env('PAYPAL_LIVE_BUTTON_ID', ''),
     'test_username' => env('PAYPAL_TEST_USERNAME', ''),
-    
+
     'sandbox_url' => 'https://www.sandbox.paypal.com/donate/?hosted_button_id=',
     'sandbox_paypal_me' => 'https://www.sandbox.paypal.com/paypalme/',
     'live_url' => 'https://www.paypal.com/donate/?hosted_button_id=',
