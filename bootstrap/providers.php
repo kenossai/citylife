5818c38d48a61b41addfdab67adbbc04abd1f480
Kenneth Ossai <kenossai1@gmail.com>
2025-12-05 23:18:39 +0000

    Remove LaravelCloudStorageServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\SessionAuthProvider::class,
-    App\Providers\LaravelCloudStorageServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
 ];
