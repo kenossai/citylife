bae98bee019c5cc0de704f3d58355864ad42ab04
Kenneth Ossai <kenossai1@gmail.com>
2025-12-05 22:43:42 +0000

    Enable S3 storage for production to persist uploaded files

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'public'),
+    'default' => env('FILESYSTEM_DISK', env('APP_ENV') === 'production' ? 's3' : 'public'),
 
     /*
     |--------------------------------------------------------------------------
