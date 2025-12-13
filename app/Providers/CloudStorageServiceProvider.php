25c0864121801913c10d7afb9235f5dbb61eba43
Kenneth Ossai <kenossai1@gmail.com>
2025-12-13 16:56:49 +0000

    Fix R2 public URL generation for image loading

--- app/Providers/CloudStorageServiceProvider.php
+++ app/Providers/CloudStorageServiceProvider.php
@@ -12,13 +12,13 @@ class CloudStorageServiceProvider extends ServiceProvider
     {
         try {
             $diskConfig = env('LARAVEL_CLOUD_DISK_CONFIG');
-            
+
             if (!$diskConfig) {
                 return;
             }
 
             $disks = json_decode($diskConfig, true);
-            
+
             if (!is_array($disks) || json_last_error() !== JSON_ERROR_NONE) {
                 Log::warning('Failed to parse LARAVEL_CLOUD_DISK_CONFIG');
                 return;
@@ -30,20 +30,31 @@ class CloudStorageServiceProvider extends ServiceProvider
                 }
 
                 $diskName = $diskData['disk'];
-                
+
                 // Configure the R2 disk
-                Config::set("filesystems.disks.{$diskName}", [
+                $config = [
                     'driver' => 's3',
                     'key' => $diskData['access_key_id'] ?? '',
                     'secret' => $diskData['access_key_secret'] ?? '',
                     'region' => $diskData['default_region'] ?? 'auto',
                     'bucket' => $diskData['bucket'] ?? '',
-                    'url' => $diskData['url'] ?? null,
                     'endpoint' => $diskData['endpoint'] ?? null,
                     'use_path_style_endpoint' => $diskData['use_path_style_endpoint'] ?? false,
                     'throw' => false,
                     'visibility' => 'public',
-                ]);
+                ];
+
+                // If URL is not provided, construct it from endpoint and bucket
+                if (!empty($diskData['url'])) {
+                    $config['url'] = $diskData['url'];
+                } elseif (!empty($diskData['endpoint']) && !empty($diskData['bucket'])) {
+                    // For R2, construct public URL from bucket name
+                    $bucket = $diskData['bucket'];
+                    $accountId = explode('.', parse_url($diskData['endpoint'], PHP_URL_HOST))[0] ?? '';
+                    $config['url'] = "https://{$bucket}.{$accountId}.r2.cloudflarestorage.com";
+                }
+
+                Config::set("filesystems.disks.{$diskName}", $config);
 
                 // Set as default if specified
                 if (!empty($diskData['is_default'])) {
