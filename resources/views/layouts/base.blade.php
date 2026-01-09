192af3490683d8d091893233934f79dd0befc181
Kenneth Ossai <kenossai1@gmail.com>
2026-01-09 13:49:37 +0000

    Update favicon images and references in layout
    Replaced the apple-touch-icon and added a new favicon.png. Updated base.blade.php to reference favicon.png for both 32x32 and 16x16 sizes, consolidating favicon usage.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,8 +10,8 @@
     <title>@yield('title') | {{ config('app.name', 'City Life International Church') }}</title>
     {{-- FAVICON ICONS --}}
     <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/favicon-16x16.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/favicon.png" />
     <link rel="manifest" href="assets/images/favicons/site.webmanifest" />
     <meta name="description" content="City Life is a vibrant spirit filled multi-cultural church affiliated with the Assemblies of God. We are situated at the heart of Kelham Island which is known for its heritage linked to steel industries." />
 
